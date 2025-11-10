77616c626321a6bfbf46c22d182433dcf72af34f
Theko Joseph <thekothekolerato@gmail.com>
2025-11-10 15:14:16 +0200

    Minor enhancements 4

--- primeai-analytics/app/layout.tsx
+++ primeai-analytics/app/layout.tsx
@@ -7,6 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 
 
+
 // Load Poppins from Google Fonts
 const poppins = Poppins({
   subsets: ["latin"],
